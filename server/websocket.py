eb1116b7acf0003b4e348682c430cdd40fc1ccfb
SerQuicky <Michael.frank96@web.de>
2020-09-26 14:09:55 +0200

    Fix the max package socket problem

--- server/websocket.py
+++ server/websocket.py
@@ -1,15 +1,17 @@
 from flask import Flask 
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO
+from engineio.payload import Payload
 import base64
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
+Payload.max_decode_packets = 500
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('transfer')
 def handleImage(msg):
 	print(type(msg))
-	
+
 	# This was only for a base64 decoding test
 	# with open("Output.txt", "w") as text_file:
     #		text_file.write(msg)
